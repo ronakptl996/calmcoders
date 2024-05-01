8b57cb1deac98b13c182c3540e3798e08f06112b
Patel Ronak <pronak391@gmail.com>
2024-05-01 23:44:21 +0530

    style: navbar & home hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,37 @@
-
 export default function Home() {
-  return <p>Test</p>;
+  return (
+    <section className="mt-20">
+      <section className="home-hero-section bg-slate-600 relative">
+        <section className="bg-hero-section"></section>
+        <section className="h-full hero-section-wrapper flex justify-center items-center flex-col px-60">
+          <h1 className="text-4xl font-bold">
+            <span className="text-lime-600">
+              Top-Rated Digital Marketing Agency
+            </span>{" "}
+            In Ahmedabad India.
+          </h1>
+          <p className="my-7 text-lg leading-8">
+            Calm Coders is a{" "}
+            <b>Best Digital Marketing Agency In Ahmedabad, India.</b> We work
+            with top brands and companies. We’ll help you hatch a strategy for
+            success that merges all channels with correlating creative and a
+            single, cohesive message to your audiences. we know that the best
+            results come from having the right people working on the right
+            assignment. With our verified strategies, your business is bound for
+            incredible success. Let’s discover how we can transform the business
+            the way you do.
+          </p>
+
+          <section className="flex">
+            <button className="bg-lime-400 hover:bg-transparent hover:border-slate-100 hover:border border border-lime-400 hover:text-lime-300 transition-colors uppercase text-sm tracking-wide py-3 px-7 rounded-3xl mr-2">
+              Free Consultation
+            </button>
+            <button className="bg-white hover:bg-lime-400 hover:border-lime-400 border border-white hover:text-white uppercase text-sm tracking-wide transition-all py-3 px-7 rounded-3xl mr-2">
+              Request SEO Audit
+            </button>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
 }
--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -28,7 +28,7 @@ export const MenuItem = ({
     <div onMouseEnter={() => setActive(item)} className="relative ">
       <motion.p
         transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
+        className="cursor-pointer text-black hover:opacity-[0.9]"
       >
         {item}
       </motion.p>
@@ -43,7 +43,7 @@ export const MenuItem = ({
               <motion.div
                 transition={transition}
                 layoutId="active" // layoutId ensures smooth animation
-                className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
+                className="bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-white/[0.2] shadow-xl"
               >
                 <motion.div
                   layout // layout ensures smooth animation
@@ -70,7 +70,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative boder border-transparent bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 "
+      className="relative boder border-white/[0.2] shadow-input flex justify-center items-center space-x-4 px-8 py-6 "
     >
       {children}
     </nav>
@@ -98,12 +98,8 @@ export const ProductItem = ({
         className="flex-shrink-0 rounded-md shadow-2xl"
       />
       <div>
-        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
-          {title}
-        </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
-          {description}
-        </p>
+        <h4 className="text-xl font-bold mb-1 text-black">{title}</h4>
+        <p className="text-neutral-700 text-sm max-w-[10rem]">{description}</p>
       </div>
     </Link>
   );
@@ -111,10 +107,7 @@ export const ProductItem = ({
 
 export const HoveredLink = ({ children, ...rest }: any) => {
   return (
-    <Link
-      {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-red-700 "
-    >
+    <Link {...rest} className="text-neutral-200 hover:text-neutral-400">
       {children}
     </Link>
   );
